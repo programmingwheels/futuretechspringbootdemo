f6a544874aea28cd4a338c0e6175f286b436d4d7
programmingwheels <leninlawrence111@gmail.com>
2022-05-15 09:53:32 +0530

    Registration API is done

--- src/main/java/com/futuretech/springbootdemo/HelloWorldController.java
+++ src/main/java/com/futuretech/springbootdemo/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.futuretech.springbootdemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class HelloWorldController {
     @GetMapping("/welcome")
     public String getWelcome(){
         return "Welcome to india";
+    }
+
+    @PostMapping("/registration")
+    public String regoister(){
+        return "Registration Successfull";
     }
 
 
